ee5e6593b17fc0271524d70adf340d232c0b5ee8
Bennett <bennettcheung@yahoo.com>
2018-08-13 16:05:43 -0700

    Added table cells implementation

---
+++ Ratings/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  Ratings
+//
+//  Created by Bennett on 2018-08-13.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
---
+++ Ratings/PlayersViewController.h
@@ -0,0 +1,14 @@
+//
+//  PlayersViewController.h
+//  Ratings
+//
+//  Created by Bennett on 2018-08-13.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *players;
+@end
