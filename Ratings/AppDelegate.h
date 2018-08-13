975d320c3b3503c2e919c529f3c2b18597a3eb7b
Bennett <bennettcheung@yahoo.com>
2018-08-13 15:28:02 -0700

    Initial Commit

---
+++ Ratings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ratings
+//
+//  Created by Bennett on 2018-08-13.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
